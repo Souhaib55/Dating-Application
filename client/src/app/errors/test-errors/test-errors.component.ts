255820f1092c109d7cbeccfcca729dba4b6edc20
Souhaib <souhaib.ammani55@gmail.com>
2025-02-10 20:33:20 +0100

    End of Section 7

---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  imports: [CommonModule],
+  templateUrl: './test-errors.component.html',
+  styleUrl: './test-errors.component.css'
+})
+export class TestErrorsComponent {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+
+  validationErrors: string[] = [];
+
+  get400Error(){
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get401Error(){
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get404Error(){
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get500Error(){
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get400ValidationError(){
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe({
+      next:response =>  console.log(response),
+      error : error => {
+      console.log(error);
+      this.validationErrors = error;
+      }
+    })
+  }
+
+
+
+}
